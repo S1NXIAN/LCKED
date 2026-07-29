6a5a2cd296b6d3f8bc3ff055a466b76021f68582
Z User <z@container>
2026-07-29 21:45:19 +0000

    04bcacce-658e-45b0-ba8b-bff2f0eb14e7

--- scripts/dev-watchdog.sh
+++ scripts/dev-watchdog.sh
@@ -3,13 +3,12 @@ cd /home/z/my-project
 while true; do
   if ! pgrep -f "next-server" > /dev/null 2>&1; then
     echo "[$(date)] Restarting..." >> /home/z/my-project/dev-watchdog.log
-    NODE_OPTIONS="--max-old-space-size=1024" bun next dev --webpack -p 3000 >> /home/z/my-project/dev.log 2>&1 &
+    NODE_OPTIONS="--max-old-space-size=768" bun next dev --webpack -p 3000 >> /home/z/my-project/dev.log 2>&1 &
     PID=$!
-    echo "[$(date)] Started PID $PID" >> /home/z/my-project/dev-watchdog.log
     wait $PID 2>/dev/null
-    echo "[$(date)] Server exited" >> /home/z/my-project/dev-watchdog.log
-    sleep 3
+    echo "[$(date)] Server exited (OOM?), waiting 5s..." >> /home/z/my-project/dev-watchdog.log
+    sleep 5
   else
-    sleep 10
+    sleep 8
   fi
 done
